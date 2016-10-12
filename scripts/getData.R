74791a9241cd68cd65b9235212725d2744d46bfc
hrecht <hrecht@users.noreply.github.com>
2016-10-12 12:02:12 -0400

    add R functionality for malformed IPEDS CSVs and throw print statments if malformed

--- scripts/getData.R
+++ scripts/getData.R
@@ -9,12 +9,15 @@ ipedspath <- "/Users/hrecht/Documents/ipeds-scraper/"
 allfiles <- fromJSON(paste(ipedspath, "data/ipedsfiles.json", sep=""))
 datacols <- fromJSON(paste(ipedspath, "data/ipedscolumns.json", sep=""))
 
+# IPEDS dictionary
+dictionary <- read.csv(paste(ipedspath, "data/dictionary.csv", sep=""), stringsAsFactors = F)
+
 # Join colnames to file info, remove FLAGS datasets, using 1990+
 ipeds <- left_join(datacols, allfiles, by = c("name", "year"))
 ipeds <- ipeds %>% filter(!grepl("flags", name)) %>%
   filter(year >= 1990)
 
-# There are a few in the way that IPEDS lists its files - remove them
+# There are a few duplicates in the way that IPEDS lists its files - remove them
 ipeds <-ipeds[!duplicated(ipeds[,"path"]),]
 
 # Search for a variable(s), return list of files that contain it
@@ -26,13 +29,32 @@ searchVars <- function(vars) {
 }
 
 # Return the datasets containing the var(s) and selected the necessary columns
-getData <- function(datalist, vars) {
+getData <- function(datalist, vars, keepallvars) {
   allvars <- tolower(c(vars, "unitid", "year"))
   for (i in seq_along(datalist)) {
+    # Construct path to CSV
     csvpath <- datalist[[i]]$path
     fullpath <- paste(ipedspath, csvpath, sep="")
     name <- datalist[[i]]$name
-    d <- read.csv(fullpath, header=T, stringsAsFactors = F, na.strings=c("",".","NA"))
+    
+    print(paste("Reading in ", fullpath, sep = ""))
+    
+    # Read CSV - some IPEDS CSVs are malformed, containing extra commas at the end of all rows but the headers
+    # Need to handle these. Permanent solution - send list of malformed files to NCES. This is a known issue.
+    row1 <- readLines(fullpath, n = 1)
+    csvnames <- unlist(strsplit(row1,','))
+    d <- read.table(fullpath, header = F, stringsAsFactors = F, sep=",", skip = 1, na.strings=c("",".","NA"))
+    if (length(csvnames) == ncol(d)) {
+      colnames(d) <- csvnames
+    } else if (length(csvnames) == ncol(d) - 1) {
+      colnames(d) <- c(csvnames, "xxx")
+      print("Malformed CSV - extra column without header. Handled by R function but note for NCES.")
+    } else if ((length(csvnames) != ncol(d) - 1) & length(csvnames) == ncol(d)) {
+      print("Malformed CSV - unknown column length mismatch error. Note for NCES")
+      print(path)
+    }
+    
+    #d <- read.csv(fullpath, header=T, stringsAsFactors = F, na.strings=c("",".","NA"))
     # Give it a year variable
     d$year <- datalist[[i]]$year
     # All lowercase colnames
@@ -43,10 +65,19 @@ getData <- function(datalist, vars) {
     {
       d$opeid <- as.character(d$opeid)
     }
-    
+    if("f2a20" %in% colnames(d))
+    {
+      d$f2a20 <- as.character(d$f2a20)
+    }
+    # unitid sometimes has type issues
+    d$unitid <- as.character(d$unitid)
     # Select just the need vars
-    selects <- intersect(colnames(d), allvars)
-    d <- d %>% select(one_of(selects))
+    if(keepallvars == FALSE) {
+      selects <- intersect(colnames(d), allvars)
+      d <- d %>% select(one_of(selects))
+    } else {
+      d <- d %>% select(-starts_with("x"))
+    }
     assign(name, d, envir = .GlobalEnv)
   }
 }
@@ -57,13 +88,15 @@ makeDataset <- function(vars) {
   ipeds_list <- lapply(dt$name, get)
   ipedsdata <- bind_rows(ipeds_list)
   ipedsdata <- ipedsdata %>% arrange(year, unitid)
+  # Unit id back to numeric
+  ipedsdata$unitid <- as.numeric(ipedsdata$unitid)
   return(ipedsdata)
 }
 
 # If desired (usually the case): Do all the things: search, get datasets
-returnData <- function(myvars) {
+returnData <- function(myvars, keepallvars = FALSE) {
   dl <- searchVars(myvars)
-  getData(dl, myvars)
+  getData(dl, myvars, keepallvars)
   makeDataset(myvars)
 }
 rm(allfiles, datacols)
